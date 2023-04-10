acfd8559ff72fd87837da4c042c1e3b47f2ed163
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-10 23:15:42 +1200

    tailwind formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,8 @@ module.exports = {
         destinationBackground:
           "url(/assets/destination/background-destination-mobile.jpg)",
         crewBackground: "url(/assets/crew/background-crew-mobile.jpg)",
-        technologyBackground: 'url("/assets/technology/background-technology-mobile.jpg")',
+        technologyBackground:
+          'url("/assets/technology/background-technology-mobile.jpg")',
       },
       boxShadow: {
         buttonShadow: "0px 0px 0px 40px rgba(255,255,255, 0.1)",
